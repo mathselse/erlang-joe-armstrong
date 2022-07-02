1b9df20a2741f97aa48b35fbf61f948734625bea
MATH El <imloneeye@gmail.com>
2022-07-02 20:56:39 +0700

    docs: modify shop1.erl to cal total cost using lists:sum & map

--- shopping/shop1.erl
+++ shopping/shop1.erl
@@ -1,9 +1,17 @@
 -module shop1.
--export [total/1, cost/1].
+-export [total/1, total2/1, cost/1].
+-import (lists, [sum/1, map/2]).
+
+% sampe product list: [{apple, 10}, {pear, 5}]
 
 total([{ProductName, NumberOfProducts}|T]) -> 
     cost(ProductName) * NumberOfProducts + total(T);
 total([]) -> 0.
+
+% total using map and list
+total2(ProductLists) ->
+    sum(map(fun({ProductName, NumberOfProducts}) -> cost(ProductName) * NumberOfProducts end, 
+        ProductLists)).
 
 cost(apple) -> 9;
 cost(pear) -> 10;
