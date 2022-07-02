b4e830b9c5cde35223354d7e3841914d390c5cf8
MATH El <imloneeye@gmail.com>
2022-07-02 19:14:56 +0700

    docs: modify lib_oca to add for with fun

--- control_abstraction/lib_oca.erl
+++ control_abstraction/lib_oca.erl
@@ -1,8 +1,14 @@
 -module lib_oca.
--export [for/2].
+-export [for/2, for/3].
 
 % to generate list of nmber min to Max
 for(Max, Max) ->
     [Max];
 for(Min, Max) ->
     [Min|for(Min+1, Max)].
+
+% to generate list of nmber min to max based on fun.
+for(Max, Max, F) ->
+    [F(Max)];
+for(Min, Max, F) ->
+    [F(Min)|for(Min+1, Max, F)].
