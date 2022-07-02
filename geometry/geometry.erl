1505253bb3aaf2770c1532079dcd565a2089400e
MATH El <imloneeye@gmail.com>
2022-07-02 17:30:24 +0700

    docs: add test to geometry.erl

--- geometry/geometry.erl
+++ geometry/geometry.erl
@@ -1,5 +1,10 @@
 -module geometry.
--export [area/1].
+-export [area/1, test/0].
+
+test() ->
+    12 = area({rectangle, 3, 4}),
+    4 = area({sequare, 2}),
+    "Test success".
 
 area({rectangle, Height, Width}) ->
     Height * Width;
