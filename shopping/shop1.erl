6876e86401ef11df0fe31c46eaad90114b20702a
MATH El <imloneeye@gmail.com>
2022-07-02 21:43:24 +0700

    docs: modify shop1.erl to total cost using list comprehension

--- shopping/shop1.erl
+++ shopping/shop1.erl
@@ -1,5 +1,5 @@
 -module shop1.
--export [total/1, total2/1, cost/1].
+-export [total/1, total2/1, total3/1, cost/1].
 -import (lists, [sum/1, map/2]).
 
 % sampe product list: [{apple, 10}, {pear, 5}]
@@ -8,10 +8,14 @@ total([{ProductName, NumberOfProducts}|T]) ->
     cost(ProductName) * NumberOfProducts + total(T);
 total([]) -> 0.
 
-% total using map and list
+% total using lists:sum & map and fun
 total2(ProductLists) ->
     sum(map(fun({ProductName, NumberOfProducts}) -> cost(ProductName) * NumberOfProducts end, 
         ProductLists)).
+
+% total using list comprehension
+total3(ProductLists) -> 
+    sum([cost(ProductName) * NumberOfProducts || {ProductName, NumberOfProducts} <- ProductLists]).
 
 cost(apple) -> 9;
 cost(pear) -> 10;
